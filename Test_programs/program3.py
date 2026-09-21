7429a59e3337c53644e571b37069c51754292483
Sourabh Mehra <sourabh999mehra@gmail.com>
2026-09-21 22:23:31 +0530

    working on student management system

---
+++ Test_programs/program3.py
@@ -0,0 +1,17 @@
+# 🚀 CHALLENGE 3 — STUDENT MANAGEMENT SYSTEM
+
+def main_menu():
+    print("""================================
+     STUDENT MANAGEMENT SYSTEM
+================================
+
+1. Add Student
+2. View Students
+3. Search Student
+4. Find Topper
+5. Calculate Average
+6. Delete Student
+7. Exit""")
+
+def add_fun():
+    
