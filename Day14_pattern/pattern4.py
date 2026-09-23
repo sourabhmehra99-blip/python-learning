2155f1a2b6aed8e121fa40fa5fea6884867c9d1d
Sourabh Mehra <sourabh999mehra@gmail.com>
2026-09-23 18:23:46 +0530

    Decreasing star pattern

--- Day14_pattern/pattern4.py
+++ Day14_pattern/pattern4.py
@@ -1,16 +1,16 @@
-
-# row = int(input("Enter number of rows : "))
-
-# for x in range(1,row + 1):
-#     for y in range(x):
-#         print("*",end=" ")
-
-#     print()
-
+# Decreasing star pattern
 row = int(input("Enter number of rows : "))
 
 for x in range(row,0,-1):
-    for y in range(x):
+    for y in range(x): # or (for y in range(1,x+1))
         print("*",end=" ")
 
     print()
+
+# output 
+# Enter number of rows : 5
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
